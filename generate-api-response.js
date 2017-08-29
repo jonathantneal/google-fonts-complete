cb65d9777e8bd9c8e5447fd365f3ca9c5fe3b871
Thomas Grainger <tom@procensus.com>
2017-08-29 15:19:06 +0100

    eslint airbnb

--- generate-api-response.js
+++ generate-api-response.js
@@ -1,22 +1,22 @@
-var URL = 'https://www.googleapis.com/webfonts/v1/webfonts?fields=items(category%2Cfamily%2ClastModified%2Csubsets%2Cvariants%2Cversion)&key=';
+const URL = 'https://www.googleapis.com/webfonts/v1/webfonts?fields=items(category%2Cfamily%2ClastModified%2Csubsets%2Cvariants%2Cversion)&key=';
 
-var fs = require('fs');
-var https = require('https');
+const fs = require('fs');
+const https = require('https');
 
 function fetchGoogleFontsList(url, key) {
-  return new Promise(function (resolve, reject) {
-    var req = https.get(url + key, function(res) {
+  return new Promise(((resolve, reject) => {
+    const req = https.get(url + key, (res) => {
       if (res.statusCode < 200 || res.statusCode > 299) {
-         reject(new Error('Failed to load list, status code: ' + res.statusCode));
+        reject(new Error(`Failed to load list, status code: ${res.statusCode}`));
       }
 
-      var rawData = '';
+      let rawData = '';
       res.setEncoding('utf8');
 
-      res.on('data', function(chunk) { return rawData += chunk; });
-      res.on('end', function() {
+      res.on('data', chunk => rawData += chunk);
+      res.on('end', () => {
         try {
-          var list = JSON.parse(rawData);
+          const list = JSON.parse(rawData);
           resolve(list.items);
         } catch (e) {
           reject(new Error(e.message));
@@ -25,20 +25,20 @@ function fetchGoogleFontsList(url, key) {
     });
 
     // handle connection errors of the request
-    req.on('error', function(err) { return reject(err); });
-  })
+    req.on('error', err => reject(err));
+  }));
 }
 
-var key = process.argv[2];
+const key = process.argv[2];
 
 if (key === undefined) {
   console.log('\x1b[31m', 'The API Key is required!');
-  return false;
+  process.exit(1);
 }
 
 fetchGoogleFontsList(URL, key)
-  .then(function(list) {
-      fs.writeFile('api-response.json', JSON.stringify(list, null, '\t'), function () {
-        console.log('Operation complete.');
-      });
+  .then((list) => {
+    fs.writeFile('api-response.json', JSON.stringify(list, null, '\t'), () => {
+      console.log('Operation complete.');
+    });
   });
--- google-fonts-generator.js
+++ google-fonts-generator.js
@@ -1,129 +1,128 @@
 // api-response.json retrieved from: https://www.googleapis.com/webfonts/v1/webfonts?fields=items(category%2Cfamily%2ClastModified%2Csubsets%2Cvariants%2Cversion)&key={YOUR_API_KEY}
 // alternatively available from: https://developers.google.com/apis-explorer/?hl=en_US#p/webfonts/v1/webfonts.webfonts.list?fields=items(category%252Cfamily%252ClastModified%252Csubsets%252Cvariants%252Cversion)&_h=3&
 
-var exports = {};
-var fonts = require('./api-response.json');
-var fs = require('fs');
-var http = require('http');
-var postcss = require('postcss');
-var postcssProcessor = postcss();
-var promise = Promise.resolve();
-var userAgents = require('./user-agents.json');
+const exports = {};
+const fonts = require('./api-response.json');
+const fs = require('fs');
+const http = require('http');
+const postcss = require('postcss');
+
+const postcssProcessor = postcss();
+let promise = Promise.resolve();
+const userAgents = require('./user-agents.json');
 
 function getSortedObject(object) {
-	var sortedObject = {};
+  const sortedObject = {};
 
-	Object.keys(object).sort().forEach(function (key) {
-		if (object[key] instanceof Array || typeof object[key] !== 'object') {
-			sortedObject[key] = object[key];
-		} else {
-			sortedObject[key] = getSortedObject(object[key]);
-		}
-	});
+  Object.keys(object).sort().forEach((key) => {
+    if (object[key] instanceof Array || typeof object[key] !== 'object') {
+      sortedObject[key] = object[key];
+    } else {
+      sortedObject[key] = getSortedObject(object[key]);
+    }
+  });
 
-	return sortedObject;
+  return sortedObject;
 }
 
 function fontToPath(family, variant) {
-	return '/css?family=' + family.replace(/\s/g, '+') + ':' + variant;
+  return `/css?family=${family.replace(/\s/g, '+')}:${variant}`;
 }
 
 function eachFont(font) {
-	var family = font.family;
-	var variants = font.variants;
+  const family = font.family;
+  const variants = font.variants;
 
-	delete font.family;
-	delete font.variants;
+  delete font.family;
+  delete font.variants;
 
-	font.variants = {};
+  font.variants = {};
 
-	exports[family] = font;
+  exports[family] = font;
 
-	variants.forEach(function (variant) {
-		var host = 'fonts.googleapis.com';
-		var path = fontToPath(family, variant);
+  variants.forEach((variant) => {
+    const host = 'fonts.googleapis.com';
+    const path = fontToPath(family, variant);
 
-		Object.keys(userAgents).forEach(function (format) {
-			var userAgent = userAgents[format];
+    Object.keys(userAgents).forEach((format) => {
+      const userAgent = userAgents[format];
 
-			var options = {
-				host: host,
-				path: path,
-				headers: {
-					'User-Agent': userAgent
-				}
-			};
+      const options = {
+        host,
+        path,
+        headers: {
+          'User-Agent': userAgent,
+        },
+      };
 
-			promise = promise.then(function () {
-				return new Promise(function (resolve, reject) {
-					function callback(response) {
-						var css = '';
+      promise = promise.then(() => new Promise(((resolve, reject) => {
+        function callback(response) {
+          let css = '';
 
-						response.on('data', function (data) {
-							css += data;
-						});
+          response.on('data', (data) => {
+            css += data;
+          });
 
-						response.on('end', function (end) {
-							if (response.statusCode === 200) {
-								postcssProcessor.process(css).then(function (result) {
-									result.root.eachAtRule('font-face', function (rule) {
-										var fontStyle = 'normal';
-										var fontWeight = '400';
+          response.on('end', (end) => {
+            if (response.statusCode === 200) {
+              postcssProcessor.process(css).then((result) => {
+                result.root.eachAtRule('font-face', (rule) => {
+                  let fontStyle = 'normal';
+                  let fontWeight = '400';
 
-										rule.eachDecl('font-weight', function (decl) {
-											fontWeight = decl.value;
-										});
+                  rule.eachDecl('font-weight', (decl) => {
+                    fontWeight = decl.value;
+                  });
 
-										rule.eachDecl('font-style', function (decl) {
-											fontStyle = decl.value;
-										});
+                  rule.eachDecl('font-style', (decl) => {
+                    fontStyle = decl.value;
+                  });
 
-										font.variants[fontStyle] = font.variants[fontStyle] || {};
-										font.variants[fontStyle][fontWeight] = font.variants[fontStyle][fontWeight] || {
-											local: [],
-											url: {}
-										};
+                  font.variants[fontStyle] = font.variants[fontStyle] || {};
+                  font.variants[fontStyle][fontWeight] = font.variants[fontStyle][fontWeight] || {
+                    local: [],
+                    url: {},
+                  };
 
-										rule.eachDecl('src', function (decl) {
-											postcss.list.comma(decl.value).forEach(function (value) {
-												value.replace(/(local|url)\((.+?)\)/g, function (match, type, path) {
-													if (type === 'local') {
-														if (font.variants[fontStyle][fontWeight].local.indexOf(path) === -1) {
-															font.variants[fontStyle][fontWeight].local.push(path);
-														}
-													} else if (type === 'url') {
-														font.variants[fontStyle][fontWeight].url[format] = path;
-													}
-												});
-											});
-										});
+                  rule.eachDecl('src', (decl) => {
+                    postcss.list.comma(decl.value).forEach((value) => {
+                      value.replace(/(local|url)\((.+?)\)/g, (match, type, path) => {
+                        if (type === 'local') {
+                          if (font.variants[fontStyle][fontWeight].local.indexOf(path) === -1) {
+                            font.variants[fontStyle][fontWeight].local.push(path);
+                          }
+                        } else if (type === 'url') {
+                          font.variants[fontStyle][fontWeight].url[format] = path;
+                        }
+                      });
+                    });
+                  });
 
-										console.log('Captured', family, fontStyle, fontWeight, 'as', format, '...');
+                  console.log('Captured', family, fontStyle, fontWeight, 'as', format, '...');
 
-										resolve();
-									});
-								});
-							} else {
-								console.log('Rejected', family, fontStyle, fontWeight, 'as', format, '...');
+                  resolve();
+                });
+              });
+            } else {
+              console.log('Rejected', family, fontStyle, fontWeight, 'as', format, '...');
 
-								resolve();
-							}
-						});
-					}
+              resolve();
+            }
+          });
+        }
 
-					http.get(options, callback);
-				});
-			});
-		});
-	});
+        http.get(options, callback);
+      })));
+    });
+  });
 
-	return font;
+  return font;
 }
 
 function oncomplete() {
-	fs.writeFile('google-fonts.json', JSON.stringify(getSortedObject(exports), null, '\t'), function () {
-		console.log('Operation complete.');
-	});
+  fs.writeFile('google-fonts.json', JSON.stringify(getSortedObject(exports), null, '\t'), () => {
+    console.log('Operation complete.');
+  });
 }
 
 fonts.forEach(eachFont);
