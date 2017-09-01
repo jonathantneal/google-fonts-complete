ad8d2c4606e0e545b3f32a2a0c31a9c0301e7d7b
Thomas Grainger <tom@procensus.com>
2017-09-01 12:32:42 +0100

    sort keys and sets so that api-response.json is always similar

--- generate-api-response.js
+++ generate-api-response.js
@@ -1,13 +1,34 @@
 const URL = 'https://www.googleapis.com/webfonts/v1/webfonts?fields=items(category%2Cfamily%2ClastModified%2Csubsets%2Cvariants%2Cversion)&key=';
-
+const _ = require('lodash/fp');
 const fs = require('mz/fs');
 const { fetch } = require('fetch-ponyfill')({});
+
+const isArray = Array.isArray;
+
+const sort = _.sortBy(_.identity);
+
+const sortKeys = _.flow(
+  _.toPairs,
+  _.sortBy(v => v[0]),
+  _.fromPairs,
+);
+
+const sortLists = _.flow(
+  sortKeys,
+  _.mapValues(v => (isArray(v) ? sort(v) : v)),
+);
+
+const sortAllLists = _.flow(
+  _.sortBy(v => v.family),
+  _.map(sortLists),
+);
 
 async function fetchGoogleFontsList(url, key) {
   const result = await fetch(url + key);
   const json = await result.json();
   console.log('Download complete');
-  await fs.writeFile('api-response.json', JSON.stringify(json.items, null, '\t'));
+  const res = sortAllLists(json.items);
+  await fs.writeFile('api-response.json', JSON.stringify(res, null, '\t'));
 }
 
 const key = process.argv[2];
